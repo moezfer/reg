e30cc45b3329a753df3e7a34e15fdfdeabaefd20
moezfer <ferchichimoez01@gmail.com>
2022-11-11 12:09:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar';
+import Acceuil from './components/Acceuil';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className=''>
+    <div className="  sticky top-0"><Navbar/></div>
+    <div className=""> <Acceuil/> </div>
+    
+  </div>
+    
   );
 }
 
---
+++ src/components/Acceuil.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Acceuil = () => {
+  return (
+    <div className='bg-acceuil bg-cover bg-center bg-no-repeat h-screen w-screen flex flex-col justify-center content-center   ' >
+        <div className=" bg-green01  m-auto w-1/2 h-1/3  ">
+            <p className="  font-bold text-left text-white md:text-4xl ml-2 leading-relaxed ">
+            Leader de la démolition <br/>
+            et recyclage de déchets de BTP 
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Acceuil
